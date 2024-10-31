e2e6a3fc7656abda7122fb80eab8774af3b8a411
raz0x <raz0x@users.noreply.github.com>
2024-10-31 13:30:58 +1100

    Create color-modes-slim.js

---
+++ js/color-modes-slim.js
@@ -0,0 +1,16 @@
+(() => {
+  'use strict'
+
+  const getPreferredTheme = () => {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+  }
+
+  const setTheme = theme => {
+    if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.documentElement.setAttribute('data-bs-theme', 'dark')
+    } else {
+      document.documentElement.setAttribute('data-bs-theme', 'theme')
+    }
+  }
+
+})()
